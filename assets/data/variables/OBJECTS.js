bc2b2191a244648d430eb54044ef3c24931798d8
Parker <pp101socials@gmail.com>
2023-05-27 09:58:40 -0400

    Created gallery objects

--- assets/data/variables/OBJECTS.js
+++ assets/data/variables/OBJECTS.js
@@ -1,1 +1,86 @@
 // This file is used to hold some objects
+import GalleryItem from "../classes/GalleryItem";
+
+const GI_1 = new GalleryItem(
+  1,
+  "https://raw.githubusercontent.com/mxrked/flawlesscustomdesigns_CDN/main/imgs/products/product%20(1).webp"
+);
+const GI_2 = new GalleryItem(
+  2,
+  "https://raw.githubusercontent.com/mxrked/flawlesscustomdesigns_CDN/main/imgs/products/product%20(2).webp"
+);
+const GI_3 = new GalleryItem(
+  3,
+  "https://raw.githubusercontent.com/mxrked/flawlesscustomdesigns_CDN/main/imgs/products/product%20(3).webp"
+);
+const GI_4 = new GalleryItem(
+  4,
+  "https://raw.githubusercontent.com/mxrked/flawlesscustomdesigns_CDN/main/imgs/products/product%20(4).webp"
+);
+const GI_5 = new GalleryItem(
+  5,
+  "https://raw.githubusercontent.com/mxrked/flawlesscustomdesigns_CDN/main/imgs/products/product%20(5).webp"
+);
+const GI_6 = new GalleryItem(
+  6,
+  "https://raw.githubusercontent.com/mxrked/flawlesscustomdesigns_CDN/main/imgs/products/product%20(6).webp"
+);
+const GI_7 = new GalleryItem(
+  7,
+  "https://raw.githubusercontent.com/mxrked/flawlesscustomdesigns_CDN/main/imgs/products/product%20(7).webp"
+);
+const GI_8 = new GalleryItem(
+  8,
+  "https://raw.githubusercontent.com/mxrked/flawlesscustomdesigns_CDN/main/imgs/products/product%20(8).webp"
+);
+const GI_9 = new GalleryItem(
+  9,
+  "https://raw.githubusercontent.com/mxrked/flawlesscustomdesigns_CDN/main/imgs/products/product%20(9).webp"
+);
+const GI_10 = new GalleryItem(
+  10,
+  "https://raw.githubusercontent.com/mxrked/flawlesscustomdesigns_CDN/main/imgs/products/product%20(10).webp"
+);
+const GI_11 = new GalleryItem(
+  11,
+  "https://raw.githubusercontent.com/mxrked/flawlesscustomdesigns_CDN/main/imgs/products/product%20(11).webp"
+);
+const GI_12 = new GalleryItem(
+  12,
+  "https://raw.githubusercontent.com/mxrked/flawlesscustomdesigns_CDN/main/imgs/products/product%20(12).webp"
+);
+const GI_13 = new GalleryItem(
+  13,
+  "https://raw.githubusercontent.com/mxrked/flawlesscustomdesigns_CDN/main/imgs/products/product%20(13).webp"
+);
+const GI_14 = new GalleryItem(
+  14,
+  "https://raw.githubusercontent.com/mxrked/flawlesscustomdesigns_CDN/main/imgs/products/product%20(14).webp"
+);
+const GI_15 = new GalleryItem(
+  15,
+  "https://raw.githubusercontent.com/mxrked/flawlesscustomdesigns_CDN/main/imgs/products/product%20(15).webp"
+);
+const GI_16 = new GalleryItem(
+  16,
+  "https://raw.githubusercontent.com/mxrked/flawlesscustomdesigns_CDN/main/imgs/products/product%20(16).webp"
+);
+
+export {
+  GI_1,
+  GI_10,
+  GI_11,
+  GI_12,
+  GI_13,
+  GI_14,
+  GI_15,
+  GI_16,
+  GI_2,
+  GI_3,
+  GI_4,
+  GI_5,
+  GI_6,
+  GI_7,
+  GI_8,
+  GI_9,
+};
