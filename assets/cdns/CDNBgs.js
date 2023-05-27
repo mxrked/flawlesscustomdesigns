9ea1dac5580ef7bb311d428e3a6414ceccbb7220
Parker <pp101socials@gmail.com>
2023-05-27 08:38:15 -0400

    Added cdns

--- assets/cdns/CDNBgs.js
+++ assets/cdns/CDNBgs.js
@@ -6,6 +6,8 @@
 
 import { CDNBGReturn } from "./CDNReturns";
 
-const BG_GRADIENT = CDNBGReturn("all", "bg-gradient", "webp");
+const BG_INDEX_TOP = CDNBGReturn("index", "top-bg", "webp");
+const BG_INDEX_INFO = CDNBGReturn("index", "info-bg", "webp");
+const BG_INDEX_CONTACT = CDNBGReturn("index", "contact-bg", "webp");
 
-export { BG_GRADIENT };
+export { BG_INDEX_CONTACT, BG_INDEX_INFO, BG_INDEX_TOP };
--- assets/cdns/CDNImgs.js
+++ assets/cdns/CDNImgs.js
@@ -6,20 +6,4 @@
 
 import { CDNImgReturn } from "./CDNReturns";
 
-const IMG_LOTTIE_COMPUTER = CDNImgReturn("lottie", "computer", "json");
-const IMG_LOTTIE_DRAWING = CDNImgReturn("lottie", "drawing", "json");
-const IMG_LOTTIE_MOBILE_DEV = CDNImgReturn("lottie", "mobile-dev", "json");
-const IMG_LOTTIE_REACHING_OUT = CDNImgReturn("lottie", "reaching-out", "json");
-const IMG_LOTTIE_WEB_DEVELOPER = CDNImgReturn(
-  "lottie",
-  "web-developer",
-  "json"
-);
-
-export {
-  IMG_LOTTIE_COMPUTER,
-  IMG_LOTTIE_DRAWING,
-  IMG_LOTTIE_MOBILE_DEV,
-  IMG_LOTTIE_REACHING_OUT,
-  IMG_LOTTIE_WEB_DEVELOPER,
-};
+export {};
